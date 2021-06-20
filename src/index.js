4d21d8fd14d708f6c3f6fb5f715765bdc778bb9a
kylieanyce <kylieanyce@gmail.com>
2021-06-20 16:46:50 -0500

    working on collapsable navbar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom"
 import { NashSSHub } from "./components/NashSSHub.js"
 import './index.css';
 import moment from "moment";
-
 
 ReactDOM.render(
   <React.StrictMode>
