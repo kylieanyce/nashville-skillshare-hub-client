2590ce329a20ed794e0fc7adf14eaa4c3dfe5318
kylieanyce <kylieanyce@gmail.com>
2021-06-24 11:19:22 -0500

    switched providers

--- src/components/events/EventProvider.js
+++ src/components/events/EventProvider.js
@@ -8,129 +8,8 @@ export const EventProvider = (props) => {
     const [searchTerms, setSearchTerms] = useState("")
 
 
-    // const getEvents = () => {
-    //     return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events`, {
-    //         headers: {
-    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
-    //         }
-    //     })
-    //         .then(response => response.json())
-    //         .then(setEvents)
-    // }
-
-
-    // const searchEvents = (searchTerm) => {
-    //     return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events?q=${searchTerm}`, {
-    //         headers: {
-    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
-    //         }
-    //     })
-    //         .then(response => response.json())
-    //         .then(setEvents)
-    // }
-
-
-    // const searchDate = (searchDate) => {
-    //     return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events?date=${searchDate}`, {
-    //         headers: {
-    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
-    //         }
-    //     })
-    //         .then(response => response.json())
-    //         .then(setEvents)
-    // }
-
-    // const getEventById = (id) => {
-    //     return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events/${id}`, {
-    //         headers: {
-    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
-    //         }
-    //     })
-    //         .then(response => response.json())
-    // }
-
-    // const getMyEvents = () => {
-    //     return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events/myevents`, {
-    //         headers: {
-    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
-    //         }
-    //     })
-    //         .then(response => response.json())
-    //         .then(setEvents)
-    // }
-
-    // const getMyBookmarks = () => {
-    //     return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events/mybookmarks`, {
-    //         headers: {
-    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
-    //         }
-    //     })
-    //         .then(response => response.json())
-    //         .then(setEvents)
-    // }
-
-    // const createEvent = (event) => {
-    //     return fetch("https://nashville-skillshare-hub-serve.herokuapp.com/events", {
-    //         method: "POST",
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
-    //         },
-    //         body: JSON.stringify(event)
-    //     })
-    //         .then(response => response.json())
-    //         .then(getEvents)
-    // }
-
-    // const updateEvent = (event) => {
-    //     return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events/${event.id}`, {
-    //         method: "PUT",
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
-    //         },
-    //         body: JSON.stringify(event)
-    //     })
-    //         .then(getEvents)
-    // }
-    // const deleteEvent = (id) => {
-    //     return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events/${id}`, {
-    //         method: "DELETE",
-    //         headers: {
-    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
-    //         },
-    //     })
-    //         .then(getEvents)
-    // }
-
-
-    // const unbookmarkEvent = eventId => {
-    //     return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events/${eventId}/bookmark`, {
-    //         method: "DELETE",
-    //         headers: {
-    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
-    //         }
-    //     })
-    //         .then(getEvents)
-    // }
-
-
-    // const bookmarkEvent = eventId => {
-    //     return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events/${eventId}/bookmark`, {
-    //         method: "POST",
-    //         headers: {
-    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
-    //         }
-    //     })
-    //         .then(response => response.json())
-    //         .then(getEvents)
-    // }
-
-
-    // FETCH CALLS FOR TESTING -------------------------------------
-
     const getEvents = () => {
-        return fetch(`http://localhost:8000/events`, {
+        return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events`, {
             headers: {
                 "Authorization": `Token ${localStorage.getItem("nssh_token")}`
             }
@@ -138,8 +17,10 @@ export const EventProvider = (props) => {
             .then(response => response.json())
             .then(setEvents)
     }
+
+
     const searchEvents = (searchTerm) => {
-        return fetch(`http://localhost:8000/events?q=${searchTerm}`, {
+        return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events?q=${searchTerm}`, {
             headers: {
                 "Authorization": `Token ${localStorage.getItem("nssh_token")}`
             }
@@ -147,8 +28,10 @@ export const EventProvider = (props) => {
             .then(response => response.json())
             .then(setEvents)
     }
+
+
     const searchDate = (searchDate) => {
-        return fetch(`http://localhost:8000/events?date=${searchDate}`, {
+        return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events?date=${searchDate}`, {
             headers: {
                 "Authorization": `Token ${localStorage.getItem("nssh_token")}`
             }
@@ -156,16 +39,18 @@ export const EventProvider = (props) => {
             .then(response => response.json())
             .then(setEvents)
     }
+
     const getEventById = (id) => {
-        return fetch(`http://localhost:8000/events/${id}`, {
+        return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events/${id}`, {
             headers: {
                 "Authorization": `Token ${localStorage.getItem("nssh_token")}`
             }
         })
             .then(response => response.json())
     }
+
     const getMyEvents = () => {
-        return fetch(`http://localhost:8000/events/myevents`, {
+        return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events/myevents`, {
             headers: {
                 "Authorization": `Token ${localStorage.getItem("nssh_token")}`
             }
@@ -173,8 +58,9 @@ export const EventProvider = (props) => {
             .then(response => response.json())
             .then(setEvents)
     }
+
     const getMyBookmarks = () => {
-        return fetch(`http://localhost:8000/events/mybookmarks`, {
+        return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events/mybookmarks`, {
             headers: {
                 "Authorization": `Token ${localStorage.getItem("nssh_token")}`
             }
@@ -182,8 +68,9 @@ export const EventProvider = (props) => {
             .then(response => response.json())
             .then(setEvents)
     }
+
     const createEvent = (event) => {
-        return fetch("http://localhost:8000/events", {
+        return fetch("https://nashville-skillshare-hub-serve.herokuapp.com/events", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -194,8 +81,9 @@ export const EventProvider = (props) => {
             .then(response => response.json())
             .then(getEvents)
     }
+
     const updateEvent = (event) => {
-        return fetch(`http://localhost:8000/events/${event.id}`, {
+        return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events/${event.id}`, {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json",
@@ -206,7 +94,7 @@ export const EventProvider = (props) => {
             .then(getEvents)
     }
     const deleteEvent = (id) => {
-        return fetch(`http://localhost:8000/events/${id}`, {
+        return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events/${id}`, {
             method: "DELETE",
             headers: {
                 "Authorization": `Token ${localStorage.getItem("nssh_token")}`
@@ -214,8 +102,10 @@ export const EventProvider = (props) => {
         })
             .then(getEvents)
     }
+
+
     const unbookmarkEvent = eventId => {
-        return fetch(`http://localhost:8000/events/${eventId}/bookmark`, {
+        return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events/${eventId}/bookmark`, {
             method: "DELETE",
             headers: {
                 "Authorization": `Token ${localStorage.getItem("nssh_token")}`
@@ -224,8 +114,9 @@ export const EventProvider = (props) => {
             .then(getEvents)
     }
 
+
     const bookmarkEvent = eventId => {
-        return fetch(`http://localhost:8000/events/${eventId}/bookmark`, {
+        return fetch(`https://nashville-skillshare-hub-serve.herokuapp.com/events/${eventId}/bookmark`, {
             method: "POST",
             headers: {
                 "Authorization": `Token ${localStorage.getItem("nssh_token")}`
@@ -234,6 +125,115 @@ export const EventProvider = (props) => {
             .then(response => response.json())
             .then(getEvents)
     }
+
+
+    // FETCH CALLS FOR TESTING -------------------------------------
+
+    // const getEvents = () => {
+    //     return fetch(`http://localhost:8000/events`, {
+    //         headers: {
+    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
+    //         }
+    //     })
+    //         .then(response => response.json())
+    //         .then(setEvents)
+    // }
+    // const searchEvents = (searchTerm) => {
+    //     return fetch(`http://localhost:8000/events?q=${searchTerm}`, {
+    //         headers: {
+    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
+    //         }
+    //     })
+    //         .then(response => response.json())
+    //         .then(setEvents)
+    // }
+    // const searchDate = (searchDate) => {
+    //     return fetch(`http://localhost:8000/events?date=${searchDate}`, {
+    //         headers: {
+    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
+    //         }
+    //     })
+    //         .then(response => response.json())
+    //         .then(setEvents)
+    // }
+    // const getEventById = (id) => {
+    //     return fetch(`http://localhost:8000/events/${id}`, {
+    //         headers: {
+    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
+    //         }
+    //     })
+    //         .then(response => response.json())
+    // }
+    // const getMyEvents = () => {
+    //     return fetch(`http://localhost:8000/events/myevents`, {
+    //         headers: {
+    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
+    //         }
+    //     })
+    //         .then(response => response.json())
+    //         .then(setEvents)
+    // }
+    // const getMyBookmarks = () => {
+    //     return fetch(`http://localhost:8000/events/mybookmarks`, {
+    //         headers: {
+    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
+    //         }
+    //     })
+    //         .then(response => response.json())
+    //         .then(setEvents)
+    // }
+    // const createEvent = (event) => {
+    //     return fetch("http://localhost:8000/events", {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
+    //         },
+    //         body: JSON.stringify(event)
+    //     })
+    //         .then(response => response.json())
+    //         .then(getEvents)
+    // }
+    // const updateEvent = (event) => {
+    //     return fetch(`http://localhost:8000/events/${event.id}`, {
+    //         method: "PUT",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
+    //         },
+    //         body: JSON.stringify(event)
+    //     })
+    //         .then(getEvents)
+    // }
+    // const deleteEvent = (id) => {
+    //     return fetch(`http://localhost:8000/events/${id}`, {
+    //         method: "DELETE",
+    //         headers: {
+    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
+    //         },
+    //     })
+    //         .then(getEvents)
+    // }
+    // const unbookmarkEvent = eventId => {
+    //     return fetch(`http://localhost:8000/events/${eventId}/bookmark`, {
+    //         method: "DELETE",
+    //         headers: {
+    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
+    //         }
+    //     })
+    //         .then(getEvents)
+    // }
+
+    // const bookmarkEvent = eventId => {
+    //     return fetch(`http://localhost:8000/events/${eventId}/bookmark`, {
+    //         method: "POST",
+    //         headers: {
+    //             "Authorization": `Token ${localStorage.getItem("nssh_token")}`
+    //         }
+    //     })
+    //         .then(response => response.json())
+    //         .then(getEvents)
+    // }
 
     return (
         <EventContext.Provider value={{
